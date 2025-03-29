8b23b1f403187048c4827d5594b07f3fd0d6c0a8
hanzalah99 <hanzalahqayyum.ee@gmail.com>
2025-03-29 02:31:44 +0100

    Implement reports sevice layer

--- src/services/projectsService.ts
+++ src/services/projectsService.ts
@@ -3,32 +3,34 @@ import { Project } from '../models/projectsModel';
 import { v4 as uuidv4 } from 'uuid';
 
 export const createProject = (name: string, description: string) => {
-    const id = uuidv4();
-    const result = db.run(
-        'INSERT INTO projects (id, name, description) VALUES (@id, @name, @description)',
-        { id, name, description }
-      );
-    return result;
- };
+  const id = uuidv4();
+  const result = db.run(
+    'INSERT INTO projects (id, name, description) VALUES (@id, @name, @description)',
+    { id, name, description }
+  );
+  return result;
+};
 
 export const getAllProjects = (): Project[] => {
-    const result = db.query('SELECT * FROM projects') as Project[];
-    return result;
+  const result = db.query('SELECT * FROM projects') as Project[];
+  return result;
 };
 
 export const getProjectById = (id: string): Project[] => {
-   const result = db.query('SELECT * FROM projects WHERE id = :id', { id }) as Project[];
-   return result;
+  const result = db.query('SELECT * FROM projects WHERE id = :id', { id }) as Project[];
+  return result;
 };
 
-export const deleteProject = (id: string) => {
-    const result = db.run('DELETE FROM projects WHERE id = :id', { id });
-    return result;
- };
-
 export const updateProject = (id: string, name: string, description: string) => {
-  return db.run(
+  const result = db.run(
     'UPDATE projects SET name = @name, description = @description WHERE id = @id',
     { id, name, description }
   );
+  return result;
 };
+
+export const deleteProject = (id: string) => {
+  const result = db.run('DELETE FROM projects WHERE id = :id', { id });
+  return result;
+};
+
---
+++ src/services/reportsService.ts
@@ -0,0 +1,41 @@
+import db from '../services/db.service';
+import { Report } from '../models/reportsModel';
+import { v4 as uuidv4 } from 'uuid';
+
+export const createReport = (text: string, projectId: string) => {
+    const id = uuidv4();
+    const result = db.run(
+        'INSERT INTO reports (id, text, projectid) VALUES (@id, @text, @projectid)',
+        { id, text, projectId }
+    );
+    return result;
+};
+
+export const getAllReports = (): Report[] => {
+    const result = db.query('SELECT * FROM reports') as Report[];
+    return result;
+};
+
+export const getReportById = (id: string): Report[] => {
+    const result = db.query('SELECT * FROM reports WHERE id = :id', { id }) as Report[];
+    return result;
+};
+
+export const getReportByProjectId = (projectId: string): Report[] => {
+    const result = db.query('SELECT * FROM reports WHERE projectid = :projectId', { projectId }) as Report[];
+    return result;
+};
+
+export const updateReport = (id: string, text: string, projectid: string) => {
+    const result = db.run(
+        'UPDATE reports SET text = @text, projectid = @projectid WHERE id = @id',
+        { id, text, projectid }
+    );
+    return result;
+};
+
+export const deleteReport = (id: string) => {
+    const result = db.run('DELETE FROM reports WHERE id = :id', { id });
+    return result;
+};
+
