9e66f4491731e8b6a307cc6dab4467274739b6a9
hanzalah99 <hanzalahqayyum.ee@gmail.com>
2025-03-29 01:51:58 +0100

    Implement project routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import projectsRoutes from './routes/projectsRoutes'
 
 dotenv.config();
 
@@ -7,6 +8,14 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/', (req,res)  => {
+	res.json({ message: 'Backend project' });
+});
+
+app.use('/projects', projectsRoutes)
+
+
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
