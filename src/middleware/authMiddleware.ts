168bdbc4df4f78ad4ecbaf993e76965042868499
hanzalah99 <hanzalahqayyum.ee@gmail.com>
2025-03-31 00:07:56 +0200

    Add authentication in header

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,20 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
 // Hardcoded authentication token
-
-const SECRET_TOKEN = 'Password123'; 
+const SECRET_TOKEN = 'Password123';
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+    const queryToken = req.query.auth as string;
+    const headerToken = req.headers['authorization'];
+    const token = queryToken ? queryToken : headerToken;
 
-    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided. Access denied.' });
+    }
 
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided. Access denied.' });
-  }
+    if (token !== SECRET_TOKEN && token !== `Bearer ${SECRET_TOKEN}`) {
+        return res.status(403).json({ message: 'Forbidden. Invalid token.' });
+    }
 
-  if (token !== `Bearer ${SECRET_TOKEN}`) {
-    return res.status(403).json({ message: 'Forbidden. Invalid token.' });
-  }
-
-  next();
+    next();
 };
